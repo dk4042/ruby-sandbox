349a4ade648ec0da20c0053b2c515956043ab1b0
dk4042 <dkhamaza@uchicago.edu>
2024-10-15 22:46:47 +0000

    initial commit

---
+++ howdy.rb
@@ -0,0 +1,11 @@
+pp "hola"
+
+require "active_support/all"
+
+pp 254.ordinalize
+
+# Program that asks for a name
+pp "Hola. Como te llamas?"
+the_name = gets
+pp the_message = "Hola, " + the_name + "! Como estas?"
+puts the_message
